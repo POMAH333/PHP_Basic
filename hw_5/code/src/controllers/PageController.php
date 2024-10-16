839ccb2d514748def7bd18967d7d3a284fc4d2cf
Rachinskiy Roman <pomka333@mail.ru>
2024-10-16 15:29:34 +0700

    Home Work 5

---
+++ hw_5/code/src/controllers/PageController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controllers;
+
+class PageController extends BaseController
+{
+    protected static ?string $modelName = 'page';
+    protected static ?string $templateFolder = 'content/page';
+}
